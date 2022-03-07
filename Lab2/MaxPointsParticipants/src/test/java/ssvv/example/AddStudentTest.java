b4f55fc593caff1965b854219b6cd040fe40c548
rares <colorlord98@gmail.com>
2022-03-07 08:45:01 +0200

    Added tests

---
+++ Lab2/MaxPointsParticipants/src/test/java/ssvv/example/AddStudentTest.java
@@ -0,0 +1,16 @@
+package ssvv.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AddStudentTest {
+	@Test
+	void testAddStudentEc() {
+		assertEquals(true, false);
+	}
+
+	@Test
+	void testAddStudentBd() {
+		assertEquals(true, false);
+	}
+}
