1365ac75b34080005460b047337367840c64859e
BuildTools <colorlord98@gmail.com>
2022-03-13 20:23:17 +0200

    Beginning of test

--- Lab2/MaxPointsParticipants/src/test/java/ssvv/example/AddStudentTest.java
+++ Lab2/MaxPointsParticipants/src/test/java/ssvv/example/AddStudentTest.java
@@ -1,16 +1,53 @@
 package ssvv.example;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import ssvv.example.domain.Student;
+import ssvv.example.validation.StudentValidator;
+import ssvv.example.validation.ValidationException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AddStudentTest {
 	@Test
 	void testAddStudentEC() {
-		assertEquals(true, false);
+		Student student1 = new Student("0", "Ion", 23);
+		Student student2 = new Student("2", "Maria", 922);
+		Student student3 = new Student("127373212", "Test", 1381);
+
+		StudentValidator studentValidator = new StudentValidator();
+
+		assertThrows(ValidationException.class, () -> {
+			studentValidator.validate(student1);
+		});
+
+		assertDoesNotThrow(() -> {
+			studentValidator.validate(student2);
+		});
+
+		assertThrows(ValidationException.class, () -> {
+			studentValidator.validate(student3);
+		});
 	}
 
 	@Test
 	void testAddStudentBVA() {
-		assertEquals(true, false);
+		Student student1 = new Student("0", "Ion", 23);
+		Student student2 = new Student("2", "Maria", 922);
+		Student student3 = new Student("127373212", "Test", 1381);
+
+		StudentValidator studentValidator = new StudentValidator();
+
+		assertThrows(ValidationException.class, () -> {
+			studentValidator.validate(student1);
+		});
+
+		assertThrows(ValidationException.class, () -> {
+			studentValidator.validate(student2);
+		});
+
+		assertThrows(ValidationException.class, () -> {
+			studentValidator.validate(student3);
+		});
+
 	}
 }
